b1f79a662c7def898c15356f86732ef7fecdbe63
igniteking <khanzaidan786@gmail.com>
2024-02-20 16:14:32 +0530

    loader

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,23 @@ export default function RootLayout({
     <html lang="en">
       <Includes />
       <body className="home-main-crev main-bg">
+        <div className="loader-wrap">
+          <svg viewBox="0 0 1000 1000" preserveAspectRatio="none">
+            <path id="svg" d="M0,1005S175,995,500,995s500,5,500,5V0H0Z"></path>
+          </svg>
+
+          <div className="loader-wrap-heading">
+            <div className="load-text">
+              <span>L</span>
+              <span>o</span>
+              <span>a</span>
+              <span>d</span>
+              <span>i</span>
+              <span>n</span>
+              <span>g</span>
+            </div>
+          </div>
+        </div>
         <div className="cursor"></div>
         <div className="progress-wrap cursor-pointer">
           <svg
